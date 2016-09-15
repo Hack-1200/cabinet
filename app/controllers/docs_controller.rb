0eab34fcbaeaf018a78bd9ad7e624cdc5c7b686b
hack-1200 <ibrox9656@gmail.com>
2016-09-16 00:16:08 +0300

    change index to show current user shows

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,7 +3,7 @@ class DocsController < ApplicationController
 	before_action :find_doc, only:[:show,:edit,:update,:destroy]
 
 	def index
-		@docs=Doc.all.order("created_at DESC")
+		@docs=Doc.where(user_id: current_user)
 	end
 
 	def show 
@@ -11,11 +11,11 @@ class DocsController < ApplicationController
 	end
 
 	def new
-		@doc=current_user.docs.new
+		@doc= current_user.docs.build
 	end
 
 	def create
-		@doc=current_user.docs.new(doc_params)
+		@doc= current_user.docs.build(doc_params)
 		if @doc.save
 			redirect_to @doc
 		else
